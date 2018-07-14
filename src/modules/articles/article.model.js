1ff9c0610d3b4708f315607bedd8f1ac1af703f9
alaaalshammaa <shammaaalalaa@gmail.com>
2018-07-14 06:30:49 +0300

    Fixes Fucking Dumb Logic Mistakes

--- src/modules/articles/article.model.js
+++ src/modules/articles/article.model.js
@@ -1,169 +1,166 @@
-import mongoose, {
-    Schema
-} from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import slug from 'slug';
 import uniqueValidator from 'mongoose-unique-validator';
 import PythonShell from 'python-shell';
-import {
-    minioClient
-} from '../../services/minio.services';
+import { minioClient } from '../../services/minio.services';
 
 // schema
 
-const ArticleSchema = new Schema({
+const ArticleSchema = new Schema(
+  {
     title: {
-        type: String,
-        trim: true,
-        required: [true, 'Title is required'],
-        minlength: [3, 'Title need to be longer'],
-        maxlength: [24, 'Title need to be shorter'],
-        unique: true,
+      type: String,
+      trim: true,
+      required: [true, 'Title is required'],
+      minlength: [3, 'Title need to be longer'],
+      maxlength: [24, 'Title need to be shorter'],
+      unique: true,
     },
     text: {
-        type: String,
-        trim: true,
-        required: [true, 'Text is required'],
-        minlength: [10, 'Text need to be longer'],
+      type: String,
+      trim: true,
+      required: [true, 'Text is required'],
+      minlength: [10, 'Text need to be longer'],
     },
     summary: {
-        type: String,
-        trim: true,
-        minLength: [10, 'Summary need to be longer'],
+      type: String,
+      trim: true,
+      minLength: [10, 'Summary need to be longer'],
     },
     photo: {
-        type: String,
-        trim: true,
+      type: String,
+      trim: true,
     },
     slug: {
-        type: String,
-        trim: true,
-        lowercase: true,
+      type: String,
+      trim: true,
+      lowercase: true,
     },
     user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
+      type: Schema.Types.ObjectId,
+      ref: 'User',
     },
     favouriteCount: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     toReadFlag: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
-}, {
+  },
+  {
     timestamps: true,
-}, );
+  },
+);
 
 ArticleSchema.plugin(uniqueValidator, {
-    message: '{VALUE} already taken!',
+  message: '{VALUE} already taken!',
 });
 
-ArticleSchema.pre('validate', function (next) {
-    this._slugify();
-    next();
+ArticleSchema.pre('validate', function(next) {
+  this._slugify();
+  next();
 });
 
 ArticleSchema.methods = {
-    _slugify() {
-        this.slug = slug(this.title);
-    },
-    toJSON() {
-        return {
-            _id: this._id,
-            title: this.title,
-            text: this.text,
-            summary: this.summary,
-            createdAt: this.createdAt,
-            user: this.user,
-            favoriteCount: this.favoriteCount,
-            photo: this.photo,
-        };
-    },
-    savePhoto(photo) {
-        minioClient.putObject(
-            'mybucket',
-            photo.originalname,
-            photo.buffer,
-            'application/octet-stream',
-            function (error, etag) {
-                if (error) {
-                    return console.log(error);
-                }
-                console.log('File uploaded successfully.');
-            },
-        );
-        this.photo = photo.originalname;
-        this.save();
-    },
+  _slugify() {
+    this.slug = slug(this.title);
+  },
+  toJSON() {
+    return {
+      _id: this._id,
+      title: this.title,
+      text: this.text,
+      summary: this.summary,
+      createdAt: this.createdAt,
+      user: this.user,
+      favoriteCount: this.favoriteCount,
+      photo: this.photo,
+    };
+  },
+  savePhoto(photo) {
+    minioClient.putObject(
+      'mybucket',
+      photo.originalname,
+      photo.buffer,
+      'application/octet-stream',
+      function(error, etag) {
+        if (error) {
+          return console.log(error);
+        }
+        console.log('File uploaded successfully.');
+      },
+    );
+    this.photo = photo.originalname;
+    this.save();
+  },
 };
 
 ArticleSchema.statics = {
-    createArticle(args, user) {
-        return this.create({
-            ...args,
-            user,
-        });
-    },
-    list({
-        skip = 0,
-        limit = 5
-    } = {}) {
-        return this.find()
-            .sort({
-                createdAt: -1,
-            })
-            .skip(skip)
-            .limit(limit)
-            .populate('user');
-    },
-    summarizeText(post, title, text) {
-        console.log('inside summarizeText');
-        const shellOptions = {
-            pythonPath: 'usr/bin/python3',
-            pythonOptions: ['-u'],
-            args: [title, text],
-        };
-        const shell = new PythonShell(
-            '/Summarization/Engine/predicter.py',
-            shellOptions,
-        );
-        shell.on('message', summary => {
-            console.log(summary);
-            post.summary = summary;
-            post.save();
-        });
-        shell.end((err, code, signal) => {
-            if (err) throw err;
-            console.log('The exit code was: ', code);
-            console.log('The exit signal was: ', signal);
-            console.log('python-shell has finished excuting');
-        });
-    },
-    incFavourite(articleId) {
-        return this.findByIdAndUpdate(articleId, {
-            $inc: {
-                favouriteCount: 1,
-            },
-        });
-    },
-    decFavourite(articleId) {
-        return this.findByIdAndUpdate(articleId, {
-            $inc: {
-                favouriteCount: -1,
-            },
-        });
-    },
-    addToRead(articleId) {
-        return this.findByIdAndUpdate(articleId, {
-            toReadFlag: true,
-        });
-    },
-    removeToRead(articleId) {
-        return this.findByIdAndUpdate(articleId, {
-            toReadFlag: false,
-        });
-    },
+  createArticle(args, user) {
+    return this.create({
+      ...args,
+      user,
+    });
+  },
+  list({ skip = 0, limit = 5 } = {}) {
+    return this.find()
+      .sort({
+        createdAt: -1,
+      })
+      .skip(skip)
+      .limit(limit)
+      .populate('user');
+  },
+  summarizeText(post, title, text) {
+    console.log('inside summarizeText');
+    const shellOptions = {
+      pythonPath:
+        '/Library/Frameworks/Python.framework/Versions/3.6/bin/python3',
+      pythonOptions: ['-u'],
+      args: [title, text],
+    };
+    const shell = new PythonShell(
+      '/Summarization/Engine/predicter.py',
+      shellOptions,
+    );
+    shell.on('message', summary => {
+      console.log(summary);
+      post.summary = summary;
+      post.save();
+    });
+    shell.end((err, code, signal) => {
+      if (err) throw err;
+      console.log('The exit code was: ', code);
+      console.log('The exit signal was: ', signal);
+      console.log('python-shell has finished excuting');
+    });
+  },
+  incFavourite(articleId) {
+    return this.findByIdAndUpdate(articleId, {
+      $inc: {
+        favouriteCount: 1,
+      },
+    });
+  },
+  decFavourite(articleId) {
+    return this.findByIdAndUpdate(articleId, {
+      $inc: {
+        favouriteCount: -1,
+      },
+    });
+  },
+  addToRead(articleId) {
+    return this.findByIdAndUpdate(articleId, {
+      toReadFlag: true,
+    });
+  },
+  removeToRead(articleId) {
+    return this.findByIdAndUpdate(articleId, {
+      toReadFlag: false,
+    });
+  },
 };
 
 export default mongoose.model('Article', ArticleSchema);
